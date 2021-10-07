4b2f7d0f06db4f448ab3cde48ad2a12e11e7dba6
Sarinder Virk <sarinder.virk@oracle.com>
2021-10-07 11:58:11 -0700

    parameterized lambda

--- amplify/backend/function/S3Trigger4c8a18ac/src/index.js
+++ amplify/backend/function/S3Trigger4c8a18ac/src/index.js
@@ -22,18 +22,54 @@ Amplify Params - DO NOT EDIT */
 
 const aws = require('aws-sdk');
 const region = process.env.REGION
+const environment = process.env.ENV
 const fileTable = process.env.API_CREATEREACTAPPAUTHAMPLIFY_FILETABLE_NAME
-aws.config.update({region: region});
-var ddb = new aws.DynamoDB({apiVersion: '2012-08-10'});
+aws.config.update({ region: region });
+var ddb = new aws.DynamoDB({ apiVersion: '2012-08-10' });
 var ddb_table_name = fileTable
 var ddb_primary_key = 'id';
 
-exports.handler = async function (event) {
+function write(params, context) {
+  ddb.putItem(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    }
+    else {
+      console.log("Success", data);
+    }
+  });
+}
+
+
+exports.handler = function(event, context) { //eslint-disable-line
   console.log('Received S3 event:', JSON.stringify(event, null, 2));
+  console.log('Context:', context);
   const bucket = event.Records[0].s3.bucket.name;
   const key = event.Records[0].s3.object.key;
   console.log(`Bucket: ${bucket}`, `Key: ${key}`);
 
-  console.log(`region: ${region}`);
+  console.log(`region: ${region}`, `environment: ${environment}`);
   console.log(`ddb_table_name: ${ddb_table_name}`, `ddb_primary_key: ${ddb_primary_key}`);
+
+  var params = {
+    TableName: ddb_table_name,
+    //Item: aws.DynamoDB.Converter.input(event.arguments)
+    Item: {
+      "id": {
+        S: "123-id"
+      },
+      "name": {
+        S: "file.txt"
+      },
+      "size": {
+        N: "123"
+      }
+    },
+    ReturnConsumedCapacity: "TOTAL",
+  };
+
+  console.log('len: ' + Object.keys(event).length)
+  if (Object.keys(event).length > 0) {
+    write(params, context);
+  }
 };
