0ae47a028f0a000ebf2d8ffad485d9937b62f56b
Sarinder Virk <sarinder.virk@oracle.com>
2021-10-17 21:45:45 -0700

    simple websocket connection

--- src/components/Files.js
+++ src/components/Files.js
@@ -70,7 +70,37 @@ export default class Files extends Component {
         }
     };
 
+    setupWebsocket() {
+        let socket = new WebSocket("wss://4l3zu1gy6h.execute-api.us-west-2.amazonaws.com/devn");
+
+        socket.onopen = function(e) {
+          console.log("[open] Connection established");
+          console.log("Sending to server");
+          socket.send("My name is John");
+        };
+        
+        socket.onmessage = function(event) {
+          console.log(`[message] Data received from server: ${event.data}`);
+        };
+        
+        socket.onclose = function(event) {
+          if (event.wasClean) {
+            console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+          } else {
+            // e.g. server process killed or network down
+            // event.code is usually 1006 in this case
+            console.log('[close] Connection died');
+          }
+        };
+        
+        socket.onerror = function(error) {
+          console.log(`[error] ${error.message}`);
+        };
+    }
+
     render() {
+        this.setupWebsocket();
+
         return (
             <Fragment>
                 <h1>Files</h1>
